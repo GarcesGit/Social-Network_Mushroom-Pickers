778f183d010178959efa2b3aadb69d1900eb77ce
Natalya Koshukova <koshukovanat@gmail.com>
2024-10-18 21:32:21 +0700

    .

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import cl from "./Footer.module.css";
 
 const Footer = () => {
-    return <div className={cl.footer}>FOOTER My contacts Icons</div>;
+    return (
+        <div className={cl.footer}>
+            <a href="https://github.com/GarcesGit">
+                <img src={github_img} alt="" className={cl.social_img} />
+            </a>
+            <a href="https://t.me/NatashKosh">
+                <img src={telegram_img} alt="" className={cl.social_img} />
+            </a>
+            <p>Nataly Garces Studio &copy; 2023</p>
+        </div>
+    );
 };
 
 export default Footer;
